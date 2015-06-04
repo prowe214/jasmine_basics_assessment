46d9461e3935513c9c00222bbf5eba79e5a78a4e
Paul Cullen Rowe <prowe214@gmail.com>
2015-06-04 10:53:10 -0600

    streamlined tests and included exact assessment test values

--- spec/spec.js
+++ spec/spec.js
@@ -13,20 +13,21 @@ describe('takes a test score and returns equivalent letter grade', function() {
 describe('takes an array of test scores and returns the average', function() {
   it('#average()', function() {
     expect(code.average([48,99,89,79,100])).toEqual(83)
+    expect(code.average([90, 95, 87, 60])).toEqual(83)
   });
 });
 
 describe('takes an array of test scores and returns the average', function() {
   it('#median()', function() {
     expect(code.median([50,60,70,80,90])).toEqual(70);
-  });
-  it('#median()', function() {
     expect(code.median([50,60,70,80,90,100])).toEqual(75);
+    expect(code.median([52,80,80,86,94])).toEqual(80);
   });
 });
 
 describe('takes an array of test scores and returns the one that occurs the most', function() {
   it('#mode()', function() {
-    expect(code.mode([48,89,89,79,100])).toEqual(89)
+    expect(code.mode([48,89,89,79,100])).toEqual(89);
+    expect(code.mode([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
   });
 });
